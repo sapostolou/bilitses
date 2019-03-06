e02dda24af5b8eed022dddeb3b0bd961ae6b9e33
spiros <s.st.apostolou@gmail.com>
2019-03-06 23:47:03 +0100

    change print

--- functions.py
+++ functions.py
@@ -4,19 +4,19 @@ from math import ceil
 
 def validSolution(solution, template, candidates, value, APSP):
     if solution == None:
-        print 'no solution found'
+        print('no solution found')
         return True
     numKeys = len(solution)
     if numKeys != template.number_of_nodes():
-        print 'not valid: template nodes missing from solution',solution
+        print('not valid: template nodes missing from solution',solution)
         return False
     duplicates = [k for k,v in Counter(solution.values()).items() if v>1]
     if len(duplicates) > 0:
-        print 'not valid: duplicates found in solution',solution
+        print('not valid: duplicates found in solution',solution)
         return False
     for k in solution:
         if solution[k] not in candidates[k]:
-            print 'not valid: node assigned to position does not belong in candidate set'
+            print('not valid: node assigned to position does not belong in candidate set')
             return False
     
     templateEdges = nx.bfs_edges(template,0)
@@ -24,7 +24,7 @@ def validSolution(solution, template, candidates, value, APSP):
     for v,u in templateEdges:
         edgeWeightSum += APSP[solution[v]][solution[u]]
     if edgeWeightSum != value:
-        print 'not valid: actual value is different than reported'
+        print('not valid: actual value is different than reported')
         return False
     return True
 
